7343cab849e3e80749f1258f3b3fe3a6295dfc43
Admin <admin@Mac-Admin.local>
2017-02-24 13:47:52 +0300

    Detailed View, fields, buttons, actions. 1h

--- AppShop/DetailViewController.swift
+++ AppShop/DetailViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var productNameLabel: UILabel!
     @IBOutlet weak var priceField: UITextField!
     @IBOutlet weak var amountField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var buyButton: UIButton!
     
     var productPrice: String?
     var productData: Product?
@@ -21,11 +22,61 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        amountField.delegate = self
         productNameLabel.text = productData?.name
         priceField.text = "\(productData!.price)"
         amountField.text = "\(productData!.amount)"
         imageView.image = productData?.image
+    }
+    @IBAction func buyButtonClick(_ sender: Any) {
+        textFieldDidEndEditing(amountField)
+        if amountField.text == "0"
+        {
+            let altMessage = UIAlertController(title: "Warning", message: "Amount can`t be zero", preferredStyle: .alert)
+            
+            let alterAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            altMessage.addAction(alterAction)
+            
+            self.present(altMessage, animated: true, completion: nil)
+            return
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let aSet = NSCharacterSet(charactersIn:"0123456789").inverted
+        let compSepByCharInSet = string.components(separatedBy: aSet)
+        let numberFiltered = compSepByCharInSet.joined(separator: "")
+        return string == numberFiltered
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //        buyButton.isEnabled = true
+        
+        if amountField.text == "" || amountField.text == "0"
+        {
+            amountField.text = "0"
+            //            buyButton.isEnabled = false
+        }
+        
+        if (amountField.text?.characters.count)! > 1 && amountField.text?.characters.first == "0"
+        {
+            amountField.text = "0"
+            //            buyButton.isEnabled = false
+        }
+        
+    }
+    
+    @IBAction func amountPlusButton(_ sender: Any) {
+        amountField.text = "\(Int(amountField.text!)! + 1)"
+        textFieldDidEndEditing(amountField)
+    }
+    @IBAction func amountMinusButton(_ sender: Any) {
+        if amountField.text == "0"
+        { return }
+        
+        amountField.text = "\(Int(amountField.text!)! - 1)"
+        textFieldDidEndEditing(amountField)
     }
     //
     //        labelValuePrice.text = productData?.price
